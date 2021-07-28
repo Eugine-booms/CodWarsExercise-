3bc1675952a06088059bfe0197b35097590def77
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-07-28 14:05:31 +0300

    StartAdd project files.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using System.Net;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Solution
+{
+
+    public static class Kata
+    {
+        public static int IsInteresting(int number, List<int> awesomePhrases)
+        {
+            // Happy Coding :)
+        }
+    }
+}
---
+++ ObservableStack_should.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+namespace Solution
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void ShouldWorkTest()
+        {
+            Assert.AreEqual(0, Kata.IsInteresting(3, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(1, Kata.IsInteresting(1336, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, Kata.IsInteresting(1337, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(0, Kata.IsInteresting(11208, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(1, Kata.IsInteresting(11209, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, Kata.IsInteresting(11211, new List<int>() { 1337, 256 }));
+        }
+    }
+}
