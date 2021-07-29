93e74c0532a8b3fa311b9564cc9d97e119745ce6
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-07-29 16:00:05 +0300

    BrainLuck_is_worked

--- Class1.cs
+++ Class1.cs
@@ -7,27 +7,74 @@ using System.Collections.Generic;
 namespace Solution
 {
 
-    public class RomanNumerals
+    public static class Kata
     {
-        private const int V = 1000;
-
-        public static string ToRoman(int n)
+        public static string BrainLuck(string code, string input)
         {
-            return "jhkhk";
-        }
+            System.Text.StringBuilder sb = new System.Text.StringBuilder(input);
+            int j = 0;
+            
+            string result=string.Empty;
+            byte[] cpu=new byte [3000];
+            int brc = 0;
+            for (int i = 0; i < code.Length; ++i)
+            {
 
-        public static int FromRoman(string romanNumeral)
-        {
-            Dictionary<char, int> romanNumbers = new Dictionary<char, int>();
-            romanNumbers.Add('I', 1);
-            romanNumbers.Add('V', 5);
-            romanNumbers.Add('X', 10);
-            romanNumbers.Add('L', 50);
-            romanNumbers.Add('C', 100);
-            romanNumbers.Add('D', 500);
-            romanNumbers.Add('M', 1000);
-            int g = romanNumbers['I'];
-            return romanNumeral.Select(x=>romanNumbers[x]).Sum();
+                if (code[i] == '>')
+                    j++;
+                if (code[i] == '<')
+                    j--;
+                if (code[i] == '+')
+                    cpu[j]++;
+                if (code[i] == '-')
+                    cpu[j]--;
+                if (code[i] == '.')
+                    result += Convert.ToChar(cpu[j]);
+                if (code[i] == ',')
+                {
+                    cpu[j] = Convert.ToByte(sb[0]);
+                    sb.Remove(0, 1);
+
+                }
+                if (code[i] == '[')
+                {
+                    if (cpu[j] == 0)
+                    {
+                        ++brc;
+                        while (brc > 0)
+                        {
+                            ++i;
+                            if (code[i] == '[')
+                                ++brc;
+                            if (code[i] == ']')
+                                --brc;
+                        }
+                    }
+                    else
+                        continue;
+                }
+                else if (code[i] == ']')
+                {
+                    if (cpu[j] == 0)
+                        continue;
+                    else
+                    {
+                        if (code[i] == ']')
+                            brc++;
+                        while (brc > 0)
+                        {
+                            --i;
+                            if (code[i] == '[')
+                                brc--;
+                            if (code[i] == ']')
+                                brc++;
+                        }
+                        --i;
+                    }
+                }
+            }
+            
+            return result;
         }
     }
 }
--- ObservableStack_should.cs
+++ ObservableStack_should.cs
@@ -1,57 +1,29 @@
-﻿namespace Solution
+﻿using System;
+using NUnit.Framework;
+namespace Solution
 {
-    using NUnit.Framework;
-    using System;
-    using System.Collections.Generic;
-
-    namespace Solution
+    [TestFixture]
+    public class BrainLuckTest
     {
-        [TestFixture]
-        public class SolutionTest
+        // Echo until byte(255) encountred
+        [Test]
+        public void test1()
         {
-            //[Test]
-            //public void TestToRoman_001()
-            //{
-            //    int input = 1;
-            //    string expected = "I";
+            Assert.AreEqual("Codewars", Kata.BrainLuck(",+[-.,+]", "Codewars" + char.ConvertFromUtf32(255)), "Should return \"Codewars\" ");
+        }
 
-            //    string actual = RomanNumerals.ToRoman(input);
+        // Echo until byte(0) encountred
+        [Test]
+        public void test2()
+        {
+            Assert.AreEqual("Codewars", Kata.BrainLuck(",[.[-],]", "Codewars" + char.ConvertFromUtf32(0)), "Should return \"Codewars\" ");
+        }
 
-            //    Assert.AreEqual(expected, actual);
-            //}
-
-            //[Test]
-            //public void TestToRoman_002()
-            //{
-            //    int input = 2;
-            //    string expected = "II";
-
-            //    string actual = RomanNumerals.ToRoman(input);
-
-            //    Assert.AreEqual(expected, actual);
-            //}
-
-            [Test]
-            public void TestFromRoman_001()
-            {
-                string input = "I";
-                int expected = 1;
-
-                int actual = RomanNumerals.FromRoman(input);
-
-                Assert.AreEqual(expected, actual);
-            }
-
-            [Test]
-            public void TestFromRoman_002()
-            {
-                string input = "II";
-                int expected = 2;
-
-                int actual = RomanNumerals.FromRoman(input);
-
-                Assert.AreEqual(expected, actual);
-            }
+        // Two numbers multiplier
+        [Test]
+        public void test3()
+        {
+            Assert.AreEqual(char.ConvertFromUtf32(72), Kata.BrainLuck(",>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.", char.ConvertFromUtf32(8) + char.ConvertFromUtf32(9)), "Should return H");
         }
     }
 }
