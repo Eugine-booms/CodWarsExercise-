7e7357d7f260bb68ab8e9b61f08672ab3b897133
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-07-29 17:11:27 +0300

    NewVersion

--- Class1.cs
+++ Class1.cs
@@ -12,38 +12,35 @@ namespace Solution
         public static string DoneOrNot(int[][] board)
         {
             int i=0, j = 0;
-            int rowSum=0, collumSum = 0;
-            int[,] sum = new int[3, 9];
+            int[] sum = new int[9];
             for (i = 0; i < board.Length; i++)
             {
+                if (board[i].Sum() != 45 || board.Select(x => x[i]).Sum() != 45)
+                    return "Try again!";
                 for (j = 0; j < board[i].Length; j++)
                 {
-                    sum[0, j] += board[i][j];
-                    sum[1,i] += board[i][j];
                     if (0 <=i && i <= 2)
                     {
-                        if (0 <= j && j <= 2) sum[2, 0] += board[i][j];
-                        if (3 <= j && j <= 5) sum[2, 1] += board[i][j];
-                        if (6 <= j && j <= 8) sum[2, 2] += board[i][j];
+                        if (0 <= j && j <= 2) sum[0] += board[i][j];
+                        if (3 <= j && j <= 5) sum[1] += board[i][j];
+                        if (6 <= j && j <= 8) sum[2] += board[i][j];
                     }
                     if (3 <= i && i <= 5)
                     {
-                        if (0 <= j && j <= 2) sum[2, 3] += board[i][j];
-                        if (3 <= j && j <= 5) sum[2, 4] += board[i][j];
-                        if (6 <= j && j <= 8) sum[2, 5] += board[i][j];
+                        if (0 <= j && j <= 2) sum[3] += board[i][j];
+                        if (3 <= j && j <= 5) sum[4] += board[i][j];
+                        if (6 <= j && j <= 8) sum[5] += board[i][j];
                     }
                     if (6 <= i && i <= 8)
                     {
-                        if (0 <= j && j <= 2) sum[2, 6] += board[i][j];
-                        if (3 <= j && j <= 5) sum[2, 7] += board[i][j];
-                        if (6 <= j && j <= 8) sum[2, 8] += board[i][j];
+                        if (0 <= j && j <= 2) sum[6] += board[i][j];
+                        if (3 <= j && j <= 5) sum[7] += board[i][j];
+                        if (6 <= j && j <= 8) sum[8] += board[i][j];
                     }
                 }
             }
-            int count = 0;
             foreach (int f in sum)
             {
-                count++;
                 if (f != 45)
                     return "Try again!";
             }
