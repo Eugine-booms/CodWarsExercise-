14e7bddcbb7140b8ef63523838e65b6fba179eab
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-07-29 13:53:39 +0300

    SinglePermutations_is_worked

--- Class1.cs
+++ Class1.cs
@@ -6,14 +6,38 @@ using System.Collections.Generic;
 
 namespace Solution
 {
-    using System;
-    using System.Collections.Generic;
+    
     class Permutations
     {
+        
         public static List<string> SinglePermutations(string s)
         {
-            // Your code here!
-            return null;
+            List<string> resultListStr = new List<string>();
+            List<string> tempListStr = new List<string>();
+            int r = 0;
+            if (s.Length == 1|| s == string.Empty)
+            {
+                resultListStr.Add(s);
+                return resultListStr;
+            }
+            if(s.Length==2)
+            {
+                resultListStr.Add(s);
+                resultListStr.Add(new string ( new char[] { s[1], s[0] } ));
+                return resultListStr.Distinct().ToList(); 
+            }
+            tempListStr.Clear();
+            foreach (var v in s)
+            {
+                tempListStr.AddRange(SinglePermutations(s.Remove(r,1)));
+                    r++;
+                foreach (var str in tempListStr)
+                {
+                    resultListStr.Add(v.ToString() + str);
+                }
+                tempListStr.Clear();
+            }
+            return resultListStr.ToArray().Distinct().Where(x=>x.Length==s.Length).ToList();
         }
     }
 }
--- ObservableStack_should.cs
+++ ObservableStack_should.cs
@@ -13,14 +13,23 @@ namespace Solution
             {
                 Assert.AreEqual(new List<string> { "a" }, Permutations.SinglePermutations("a").OrderBy(x => x).ToList());
             }
+        [Test]
+        public void Example1_2()
+        {
+            Assert.AreEqual(new List<string> { "aa" }, Permutations.SinglePermutations("aa").OrderBy(x => x).ToList());
+        }
 
-            [Test]
+        [Test]
             public void Example2()
             {
-                Assert.AreEqual(new List<string> { "ab", "ba" }, Permutations.SinglePermutations("ab").OrderBy(x => x).ToList());
+                    Assert.AreEqual(new List<string> { "ab", "ba" }, Permutations.SinglePermutations("ab").OrderBy(x => x).ToList());
             }
-
-            [Test]
+        [Test]
+        public void Example2_1()
+        {
+            Assert.AreEqual(new List<string> { "abc", "acb", "bac", "bca", "cab", "cba" }, Permutations.SinglePermutations("abc").OrderBy(x => x).ToList());
+        }
+        [Test]
             public void Example3()
             {
                 Assert.AreEqual(new List<string> { "aabb", "abab", "abba", "baab", "baba", "bbaa" }, Permutations.SinglePermutations("aabb").OrderBy(x => x).ToList());
