f5e630ada1ca8a5585616da1041cdf395e2d354c
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-07-30 19:59:37 +0300

    Вроде поборол рекурсию1

--- ObservableStack_should.cs
+++ ObservableStack_should.cs
@@ -11,9 +11,9 @@ namespace Solution
         public void ShouldWorkTest()
         {
             Assert.AreEqual(0, Kata.IsInteresting(3, new List<int>() { 1337, 256 }));
-            //Assert.AreEqual(1, Kata.IsInteresting(1336, new List<int>() { 1337, 256 }));
-            Assert.AreEqual(2, Kata.IsInteresting(7890, new List<int>() { 1337, 256 }));
-            Assert.AreEqual(2, Kata.IsInteresting(0123, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(1, Kata.IsInteresting(1336, new List<int>() { 1337, 256 }));
+            ////Assert.AreEqual(2, Kata.IsInteresting(7890, new List<int>() { 1337, 256 }));
+            ////Assert.AreEqual(2, Kata.IsInteresting(0123, new List<int>() { 1337, 256 }));
             Assert.AreEqual(2, Kata.IsInteresting(1000, new List<int>() { 1337, 256 }));
             Assert.AreEqual(2, Kata.IsInteresting(1337, new List<int>() { 1337, 256 }));
             Assert.AreEqual(2, Kata.IsInteresting(11111, new List<int>() { 1337, 256 }));
