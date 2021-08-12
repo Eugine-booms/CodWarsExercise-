41180c8b61560b1bd2a8cab9bc7c75f8b3330788
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-08-12 14:19:45 +0300

    work

--- Class1.cs
+++ Class1.cs
@@ -1,33 +1,48 @@
-﻿using System;
-
-using System.Net;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Collections.Generic;
 
 namespace Solution
 {
+    using System;
 
-    public class RomanNumerals
+    public class Intervals
     {
-        private const int V = 1000;
-
-        public static string ToRoman(int n)
+        public static int SumIntervals((int, int)[] intervals)
         {
-            return "jhkhk";
-        }
+            int summ = 0;
+            var f = intervals.OrderBy(x => x.Item1).ThenBy(x => x.Item2).ToList();
+            List<int> a = new List<int>();
+            int count = f.Count;
+            for (int j = 0; j < count; j++)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (i==j) continue;
+                    if (i + 1 <= f.Count && f[j].Item1 <= f[i].Item1 && f[j].Item2 >= f[i].Item2)
+                    {
+                        count--;
+                        f.Remove(f[i]);
+                        i--;
+                    }
+                }
+            }
+            foreach (var x in f)
+            {
+                a.Add(x.Item1);
+                a.Add(x.Item2);
+            }
+            for (int i=1; i<a.Count;i+=2)
+            {
+                summ +=a[i];
+                summ -= a[i-1];
+                //summ = Math.Abs(summ);
+                if (i + 1 < a.Count && a[i] > a[i + 1])
+                    summ -= a[i] - a[i + 1];
 
-        public static int FromRoman(string romanNumeral)
-        {
-            Dictionary<char, int> romanNumbers = new Dictionary<char, int>();
-            romanNumbers.Add('I', 1);
-            romanNumbers.Add('V', 5);
-            romanNumbers.Add('X', 10);
-            romanNumbers.Add('L', 50);
-            romanNumbers.Add('C', 100);
-            romanNumbers.Add('D', 500);
-            romanNumbers.Add('M', 1000);
-            int g = romanNumbers['I'];
-            return romanNumeral.Select(x=>romanNumbers[x]).Sum();
+
+            }
+            return summ;
         }
     }
+
 }
--- ObservableStack_should.cs
+++ ObservableStack_should.cs
@@ -1,57 +1,65 @@
 ﻿namespace Solution
 {
     using NUnit.Framework;
-    using System;
-    using System.Collections.Generic;
 
-    namespace Solution
+    using Interval = System.ValueTuple<int, int>;
+
+    public class IntervalTest
     {
-        [TestFixture]
-        public class SolutionTest
+        [Test]
+        public void ShouldHandleEmptyIntervals()
         {
-            //[Test]
-            //public void TestToRoman_001()
-            //{
-            //    int input = 1;
-            //    string expected = "I";
-
-            //    string actual = RomanNumerals.ToRoman(input);
-
-            //    Assert.AreEqual(expected, actual);
-            //}
-
-            //[Test]
-            //public void TestToRoman_002()
-            //{
-            //    int input = 2;
-            //    string expected = "II";
-
-            //    string actual = RomanNumerals.ToRoman(input);
-
-            //    Assert.AreEqual(expected, actual);
-            //}
-
-            [Test]
-            public void TestFromRoman_001()
-            {
-                string input = "I";
-                int expected = 1;
-
-                int actual = RomanNumerals.FromRoman(input);
-
-                Assert.AreEqual(expected, actual);
-            }
-
-            [Test]
-            public void TestFromRoman_002()
-            {
-                string input = "II";
-                int expected = 2;
-
-                int actual = RomanNumerals.FromRoman(input);
-
-                Assert.AreEqual(expected, actual);
-            }
+            Assert.AreEqual(0, Intervals.SumIntervals(new Interval[] { }));
+            Assert.AreEqual(0, Intervals.SumIntervals(new Interval[] { (4, 4), (6, 6), (8, 8) }));
         }
-    }
+
+        [Test]
+        public void ShouldAddDisjoinedIntervals()
+        {
+            Assert.AreEqual(9, Intervals.SumIntervals(new Interval[] { (1, 2), (6, 10), (11, 15) }));
+            Assert.AreEqual(11, Intervals.SumIntervals(new Interval[] { (4, 8), (9, 10), (15, 21) }));
+            Assert.AreEqual(7, Intervals.SumIntervals(new Interval[] { (-1, 4), (-5, -3) }));
+            Assert.AreEqual(78, Intervals.SumIntervals(new Interval[] { (-245, -218), (-194, -179), (-155, -119) }));
+        }
+
+        [Test]
+        public void ShouldAddAdjacentIntervals()
+        {
+            Assert.AreEqual(54, Intervals.SumIntervals(new Interval[] { (1, 2), (2, 6), (6, 55) }));
+            Assert.AreEqual(23, Intervals.SumIntervals(new Interval[] { (-2, -1), (-1, 0), (0, 21) }));
+        }
+
+        [Test]
+        public void ShouldAddOverlappingIntervals()
+        {
+            Assert.AreEqual(7, Intervals.SumIntervals(new Interval[] { (1, 4), (7, 10), (3, 5) }));
+            Assert.AreEqual(6, Intervals.SumIntervals(new Interval[] { (5, 8), (3, 6), (1, 2) }));
+            Assert.AreEqual(19, Intervals.SumIntervals(new Interval[] { (1, 5), (10, 20), (1, 6), (16, 19), (5, 11) }));
+        }
+
+        [Test]
+        public void ShouldHandleMixedIntervals()
+        {
+            Assert.AreEqual(13, Intervals.SumIntervals(new Interval[] { (2, 5), (-1, 2), (-40, -35), (6, 8) }));
+            Assert.AreEqual(1234, Intervals.SumIntervals(new Interval[] { (-7, 8), (-2, 10), (5, 15), (2000, 3150), (-5400, -5338) }));
+            Assert.AreEqual(158, Intervals.SumIntervals(new Interval[] { (-101, 24), (-35, 27), (27, 53), (-105, 20), (-36, 26) }));
+        }
+        [Test]
+        public void ShouldHandleRandomIntervals()
+        {
+            Assert.AreEqual(19461, Intervals.SumIntervals(new Interval[] { (-6432, 9700), (-4562, 6732), (-273, 3749), (-8833, 3611), 
+                (-362, 7468), (-8341, 4846), (-9761, -8103) ,
+            (-4702, 2104), (-8280, 2864), (6201, 7444), (-4971, 4828), (7282, 7657), (-9761, -8103), (-4224, -296), (-1751, 1759),
+            (-2281, 7737), (-5417, 7391), (1370, 5183), (-3476, 7707), (-6860, -182), (-9322, -8943), (5615, 8701), (2067, 3034), (-5007, 1288)}));
+            //    (-1311, 5982) , (-4945, -1028), (3412, 9351), (-6296, -6241), (-3715, 7128), (-6460, 9460), (-5956, 956), (-8522, -8149), (-8081, -6571) }));
+            //    //Assert.AreEqual(1234, Intervals.SumIntervals(new Interval[] { (-7, 8), (-2, 10), (5, 15), (2000, 3150), (-5400, -5338) }));
+            //    //Assert.AreEqual(158, Intervals.SumIntervals(new Interval[] { (-101, 24), (-35, 27), (27, 53), (-105, 20), (-36, 26) }));
+        }
+}
+
+
+
+
+
+
 }
