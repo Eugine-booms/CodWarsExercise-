cc6a938585ac48ab5e1a1d426d9ca2e26c83a181
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-10-08 18:34:05 +0300

    Перенос в новую папку

--- Class1.cs
+++ Class1.cs
@@ -6,38 +6,22 @@ using System.Collections.Generic;
 
 namespace Solution
 {
-    
+
     class Permutations
     {
-        
-        public static List<string> SinglePermutations(string s)
+
+        public void HtmlTags(string str)
         {
-            List<string> resultListStr = new List<string>();
-            List<string> tempListStr = new List<string>();
-            int r = 0;
-            if (s.Length == 1|| s == string.Empty)
+            string[] inputs;
+            inputs = Console.ReadLine().Split(' ');
+            int num = int.Parse(inputs[0]);
+
+            for (int i = 0; i < num; i++)
             {
-                resultListStr.Add(s);
-                return resultListStr;
+                inputs = Console.ReadLine().Split(' ');
+                var s = int.Parse(inputs[0]);
+                Console.WriteLine("{0}", Math.Pow(s, 5));
             }
-            if(s.Length==2)
-            {
-                resultListStr.Add(s);
-                resultListStr.Add(new string ( new char[] { s[1], s[0] } ));
-                return resultListStr.Distinct().ToList(); 
-            }
-            tempListStr.Clear();
-            foreach (var v in s)
-            {
-                tempListStr.AddRange(SinglePermutations(s.Remove(r,1)));
-                    r++;
-                foreach (var str in tempListStr)
-                {
-                    resultListStr.Add(v.ToString() + str);
-                }
-                tempListStr.Clear();
-            }
-            return resultListStr.ToArray().Distinct().Where(x=>x.Length==s.Length).ToList();
         }
     }
 }
