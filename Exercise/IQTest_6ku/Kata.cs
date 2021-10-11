54f96f405f4d8e59556320ea6623b6ab12e8acba
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-10-11 14:40:20 +0300

    add IQTest

---
+++ Exercise/IQTest_6ku/Kata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Delegates.Observers.Exercise.IQTest_6ku
+{
+ 
+
+    public class IQ
+    {
+        public static int Test(string numbers)
+        {
+            var res = numbers.Split(' ')
+                .Select((value, ind) => new { isEven=int.Parse(value) % 2, index=ind + 1 })
+                .GroupBy(x=>x.isEven)
+                .First(x => x.Count() == 1)
+                .First()
+                .index;
+
+            
+            return res;
+        }
+    }
+}
---
+++ Test/IQTest_should.cs
@@ -0,0 +1,19 @@
+﻿namespace Delegates.Observers.Exercise.IQTest_6ku
+{
+    using NUnit.Framework;
+
+    internal class IQTest_should
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(3, IQ.Test("2 4 7 8 10"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(1, IQ.Test("1 2 2"));
+        }
+    }
+}
