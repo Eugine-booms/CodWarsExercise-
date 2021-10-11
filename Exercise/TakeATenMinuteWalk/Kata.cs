2c9599539b992c9a8cd937bc36a3f8886c35b6e7
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-10-11 13:21:30 +0300

    add TakeATenMinuteWalk

---
+++ Exercise/TakeATenMinuteWalk/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Delegates.Observers.Exercise.TakeATenMinuteWalk
+{
+    public class Kata
+    {
+        private static Dictionary<string, int> veight = new Dictionary<string, int> { {"n",1}, {"s", -1}, {"w",2},{"e",-2 } };
+        public static bool IsValidWalk(string[] walk)
+        {
+           
+            return walk.Select(x => veight[x]).Count()==10&&walk.Select(x => veight[x]).Sum() == 0;
+            //insert brilliant code here
+        }
+    }
+}
