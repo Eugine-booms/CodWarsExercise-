4cc5ba84da652ac51406fbd6f1eadfd6c6e8f8db
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-10-12 16:07:20 +0300

    add validBrace

---
+++ Exercise/Valid Braces 6ku/Brace.cs
@@ -0,0 +1,44 @@
+﻿namespace CodeWars.Exercise.Valid_Braces_6ku
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Brace
+    {
+
+        public static bool validBraces(String braces)
+        {
+            var charArray = braces.ToCharArray();
+            var stack = new Stack<char>();
+
+            foreach (var item in charArray)
+            {
+                switch (item)
+                {
+                    case '(':
+                        stack.Push(')');
+                        break;
+                    case '[':
+                        stack.Push(']');
+                        break;
+                    case '{':
+                        stack.Push('}');
+                        break;
+                    default:
+                        
+                        if (stack.Count>0&&stack.Peek() == item)
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine(braces);
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Test/ValidBraces_should.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeWars.Exercise.Valid_Braces_6ku
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BraceTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, Brace.validBraces("()"));
+        }
+        [Test]
+        public void Test2()
+        {
+
+            Assert.AreEqual(false, Brace.validBraces("[(])"));
+        }
+        [Test]
+        public void Test3()
+        {
+
+            Assert.AreEqual(false, Brace.validBraces("}}]]))}])"));
+        }
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(false, Brace.validBraces("(})"));
+        }
+    }
+
+}
