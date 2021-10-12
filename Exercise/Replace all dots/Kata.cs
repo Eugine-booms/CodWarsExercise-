5ed5a7352d5e4f538fe4ac4d0961d176690b8f72
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-10-12 15:47:13 +0300

    add ReplaceDots

---
+++ Exercise/Replace all dots/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeWars.Exercise.Replace_all_dots
+{
+    public class Kata
+    {
+        public static string ReplaceDots(string str)
+        {
+            return str.Replace(".", "-");
+        }
+    }
+}
---
+++ Test/ReplaceAllDots_should.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeWars.Exercise.Replace_all_dots
+{
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("one-two-three", Kata.ReplaceDots("one.two.three"));
+        }
+    }
+
+}
